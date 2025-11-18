a67c109db79337dfc2f309bd49d073c3fea65361
uttam97 <45070037+uttam97@users.noreply.github.com>
2025-11-18 15:48:15 +0000

    basics learned-01

--- 01_basics/beginner.js
+++ 01_basics/beginner.js
@@ -1,1 +1,18 @@
-console.log("uttam")
+const a = "5";
+var b = "uttam"
+let c = "krr"
+name = "sat"
+//a = "6"
+
+b = "uuu" 
+/*
+try to use let and const as some issue is 
+there in functional and block scope */
+
+c = "pp"
+
+name ="hhhhh"
+
+console.table([a,b,c,name])
+
+b="utt"
