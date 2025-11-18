e352cdba2d93de91d6d687a56887a385acf1b9cd
uttam97 <45070037+uttam97@users.noreply.github.com>
2025-11-18 16:03:34 +0000

    02

---
+++ basics/02_data-types.js
@@ -0,0 +1,16 @@
+"use strict"
+
+// alert (3 + 3) // using node js not browser
+
+//number
+//bigint
+//string
+//null
+//undefined
+//boolean
+// symbol => unique
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
