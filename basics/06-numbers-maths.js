621495734b22ffdca0da5bcb62e10e2b9b41afdb
uttam97 <45070037+uttam97@users.noreply.github.com>
2025-11-20 17:01:26 +0000

    Maths and number basics

---
+++ basics/06-numbers-maths.js
@@ -0,0 +1,23 @@
+const score = 400
+
+const balance = new Number(500.9888)
+
+//console.log(balance.toPrecision(3));
+
+const hundreds =1000000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+
+/////////+++++++++++++//////////////////++++++++++++////////
+
+// console.log(Math);
+// console.log(Math.abs(-4))
+console.log(Math.random())
+(Math.random() * 10) +1
+
+const max =10
+const min =20
+
+console.log(Math.random() * (max-min +1) +min)
+
+
